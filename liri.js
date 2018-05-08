96fc29d3b4836dbbbe9f44830572006cf1900dc2
gustavo2011 <gustavo.jauregui2011@gmail.com>
2018-05-07 19:11:33 -0700

    added some js to pull twitter api key

--- liri.js
+++ liri.js
@@ -1,1 +1,6 @@
+console.log("this is boomin");
 require("dotenv").config();
+var Twitter = require('twitter');
+var keys = require("./keys.js");
+var client = new Twitter(keys.twitter);
+console.log(client);
