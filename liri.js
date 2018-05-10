9106babd5dc385c641f08408ab82c21055b62372
gustavo2011 <gustavo.jauregui2011@gmail.com>
2018-05-10 13:08:45 -0700

    cleaned up code & working on spotify search

--- liri.js
+++ liri.js
@@ -5,31 +5,37 @@ var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var request = require("request");
 
+
+
+
 var action = process.argv[2];
 
-var argument = "";
+var value = process.argv[3];
 
-magicMaker (action, argument);
 
-function magicMaker(action, argument){
     switch (action) {
         case 'my-tweets':
         getTweets();
         break;
 
-    }
-}
+        case 'spotify-this-song':
+        spotifySearch();
+        
+  } 
+
+
+
 
 //get twitter status
 function getTweets() {
 var client = new Twitter(keys.twitter);
 var params = {screen_name: 'fadez510'};
+console.log("initializing function");
 
-// Shows up to last 20 tweets and when created in terminal.
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
-
-      // Loops through tweets and prints out tweet text and creation date.
+    
+    
       for (var i = 0; i < tweets.length; i++) {
           var tweetText = tweets[i].text;
           console.log("Tweet Text: " + tweetText);
@@ -42,3 +48,39 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
   });
 }
 
+
+
+//get spotify song searches
+function spotifySearch() {
+
+    console.log("initializing funciton");
+
+    if(!value) {
+        value = "The Sign";
+    }
+    var client = new Spotify(keys.spotify);
+    spotify.search({ type: 'track', query: song}, function(error, data) {
+        if (!error) {
+            for(var i= 0; i<data.tracks.items.length; i++) {
+             var songInfo = data.tracks.items[i];
+             console.log("Artist: " + songInfo.artits[0].name);
+        
+            }
+          
+        } else {
+            return console.log('Error occurred: ' + error);
+        }
+    });
+} 
+
+
+function defaultSong() {
+    var client = new Spotify(keys.spotify);
+    spotify.lookup({ type: 'track', id: '3DYVWvPh3kGwPasp7yjahc'}, function(error, data){
+        if(!error){
+            return console.log('Error occurred: ' + error);
+        } else {
+            console.log("Artist: "+ songInfo.artits[0].name);
+        }
+    });
+}
